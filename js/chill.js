2de0c93811be2c1dc9cce1207de0291057a6d533
undead747 <narutominh747@gmail.com>
2019-09-23 21:19:20 +0700

    change background

---
+++ js/chill.js
@@ -0,0 +1,180 @@
+//get API
+const client = contentful.createClient({
+    space: "np5xiu7p1a25",
+    accessToken: "rZ3ZfI6DzVOoGzafNQ5Tw3Z4iFNvP0Z1AMP-0_HPaZ8"
+});
+
+
+const imgToggleBtn = document.querySelector(".img-toggle-btn");
+const imgSideBar = document.querySelector(".image-sidebar");
+const imageSidebarSon = document.querySelector(".image-sidebar-son");
+
+class ImageData {
+    async getDatas() {
+        let contentful = await client.getEntries({
+            content_type: "imageProducts"
+        })
+
+        let data = contentful.items;
+
+        data = data.map(item => {
+            let {
+                images,
+                type
+            } = item.fields;
+
+            images = images.map(p => {
+                let {
+                    title,
+                    file
+                } = p.fields;
+                file = "https:" + file.url;
+
+                return {
+                    title,
+                    file
+                }
+            })
+
+            return {
+                images,
+                type
+            }
+        })
+        Storage.saveImages(data);
+        console.log(data)
+    }
+
+
+}
+
+class ImageUI {
+    //side bar toggle 
+    imgToggleBtn() {
+        imgToggleBtn.addEventListener("click", event => {
+            if (imgSideBar.classList.contains('active-right')) {
+                imgSideBar.classList.remove('active-right');
+            } else if (imgSideBar.classList.contains('active-image-right')) {
+                imgSideBar.classList.remove('active-image-right');
+                imageSidebarSon.classList.remove("active-right-son");
+            } else {
+                imgSideBar.classList.add('active-right');
+            }
+        })
+    }
+
+    showImageTypeList(data) {
+        let ul = document.createElement("ul");
+        data.forEach(element => {
+            let li = document.createElement("li");
+            li.classList.add("type-image-button");
+            li.innerHTML = `
+               ${element.type}
+            `;
+            ul.appendChild(li);
+        });
+
+        imgSideBar.appendChild(ul);
+    }
+
+    imageTypeListButtonToggle() {
+        const imgButtonList = [...document.querySelectorAll(".type-image-button")];
+        imgButtonList.forEach(p => {
+            p.addEventListener("click", event => {
+                this.showImageList(p.innerText);
+                this.changeSider(p.innerText);
+                this.imageToggle();
+            })
+        })
+
+    }
+
+    setBackground(icon) {
+        console.log(icon)
+        document.body.style.backgroundImage = "url(./images-music/" + icon + ")";
+    }
+
+    imageToggle() {
+        const imageBtnList = [...document.querySelectorAll(".image-button")];
+
+        imageBtnList.forEach(p => {
+            p.addEventListener("click", event => {
+                let title = p.getElementsByTagName("img")[0].alt;
+                this.setBackground(title);
+            })
+        })
+    }
+
+    changeSider(type) {
+        let check = imgSideBar.getAttribute("type");
+        if (check === null || check === type) {
+            imgSideBar.setAttribute('type', type);
+            if (imgSideBar.classList.contains("active-image-right")) {
+                imgSideBar.classList.remove("active-image-right");
+                imgSideBar.classList.add("active-right");
+            } else {
+                imgSideBar.classList.add("active-image-right");
+                imgSideBar.classList.remove("active-right");
+            }
+
+            imageSidebarSon.classList.toggle("active-right-son");
+        } else {
+            imgSideBar.setAttribute('type', type);
+            this.showImageList(type);
+        }
+    }
+
+
+    showImageList(type) {
+
+        const data = Storage.getAllData();
+
+        let images = data.filter(p => {
+            return p.type === type
+        })
+
+        images = images[0].images;
+        let ul = "";
+
+        images.forEach(p => {
+            ul += `
+            <li class="image-button">   
+            <img src="${p.file}" alt="${p.title}" width="300px" height="180px">
+            </li>
+            `;
+        })
+
+        imageSidebarSon.innerHTML = `
+         <ul></ul>
+        `;
+
+        imageSidebarSon.getElementsByTagName("ul")[0].innerHTML = ul;
+    }
+}
+
+class Storage {
+    static saveImages(data) {
+        localStorage.setItem("images", JSON.stringify(data));
+    }
+
+    static getAllData() {
+        const data = JSON.parse(localStorage.getItem("images"));
+        return data;
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUI = new ImageUI();
+    const imageData = new ImageData();
+
+    imgUI.imgToggleBtn();
+    imageData.getDatas().then(() => {
+        imgUI.showImageTypeList(Storage.getAllData());
+    }).then(() => {
+        imgUI.imageTypeListButtonToggle();
+    }).then(() => {
+        imgUI.imageToggle();
+    })
+
+})
--- js/weather.js
+++ js/weather.js
@@ -6,105 +6,119 @@ const degreeSection = document.querySelector(".degree-section");
 const typeTeperature = document.querySelector("#type-teperature");
 const currentDay = document.querySelector(".current-day");
 const currentDate = document.querySelector(".current-date");
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const weatherIconList = ["clear-day", "clear-night", "partly-cloudy-day", "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind", "fog"];
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+const weatherIconList = [
+  "clear-day",
+  "clear-night",
+  "partly-cloudy-day",
+  "partly-cloudy-night",
+  "cloudy",
+  "rain",
+  "sleet",
+  "snow",
+  "wind",
+  "fog"
+];
 
 class UI {
-    setIcon(icon, iconID) {
-        const skycons = new Skycons({
-            color: "black"
-        });
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        console.log(icon)
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]);
-    }
+  setIcon(icon, iconID) {
+    const skycons = new Skycons({
+      color: "black"
+    });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    console.log(icon);
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
 
-    setBackground(icon) {
-        document.body.style.backgroundImage = "url('../images/" + icon + ".jpg')";
+  setBackground(icon) {
+    document.body.style.backgroundImage = "url(./images/" + icon + ".jpg)";
+  }
 
-    }
+  showUI(data) {
+    console.log(data);
 
-    showUI(data) {
+    const {
+      temperature,
+      summary,
+      icon
+    } = data.hourly.data[0];
 
-        const {
-            temperature,
-            summary,
-            icon
-        } = data.currently;
+    let day = new Date();
 
-        const dailySummary = data.daily.summary;
+    let today =
+      day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDate();
 
-        let day = new Date();
+    let temperatureInC;
+    let timeZone = data.timezone.replace("/", " - ");
+    timeZone = timeZone.replace(/_/g, " ");
 
-        let today = day.getFullYear() + '-' + (day.getMonth() + 1) + "-" + day.getDate();
+    currentDay.textContent = weekday[day.getDay()];
+    temperatureInC = Math.floor((temperature - 32) / 1.8);
+    tempeDegree.textContent = temperatureInC;
+    tempeDiscription.textContent = summary;
+    locationTimeZone.textContent = timeZone;
+    currentDate.textContent = today;
 
-        let temperatureInC;
-        let timeZone = data.timezone.replace("/", " - ");
-        timeZone = timeZone.replace(/_/g, " ");
-
-        currentDay.textContent = weekday[day.getDay()];
-        temperatureInC = Math.floor((temperature - 32) / 1.8);
+    degreeSection.addEventListener("click", event => {
+      if (typeTeperature.textContent === "C") {
+        typeTeperature.textContent = "F";
+        tempeDegree.textContent = temperature;
+      } else {
+        typeTeperature.textContent = "C";
         tempeDegree.textContent = temperatureInC;
-        tempeDiscription.textContent = summary;
-        locationTimeZone.textContent = timeZone;
-        currentDate.textContent = today;
+      }
+    });
 
-        degreeSection.addEventListener('click', event => {
-            if (typeTeperature.textContent === 'C') {
-                typeTeperature.textContent = 'F';
-                tempeDegree.textContent = temperature;
-            } else {
-                typeTeperature.textContent = 'C';
-                tempeDegree.textContent = temperatureInC;
-            }
-        })
+    this.setIcon(icon, weatherIcon);
+    this.setBackground(icon);
+  }
 
+  async getAPI(position) {
+    let long, lat;
+    const ui = new UI();
 
-        this.setIcon(icon, weatherIcon);
-        this.setBackground(icon);
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api = `${proxy}https://api.darksky.net/forecast/f220a03da5d612681fdfc1efc311c15c/${lat},${long}`;
+
+    try {
+      const response = await fetch(api);
+      const data = await response.json();
+
+      Storage.saveData(data);
+    } catch (e) {
+      console.log(e);
     }
-
-    async getAPI(position) {
-        let long, lat;
-
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-
-        const proxy = "https://cors-anywhere.herokuapp.com/";
-        const api = `${proxy}https://api.darksky.net/forecast/f220a03da5d612681fdfc1efc311c15c/${lat},${long}`;
-
-        try {
-            const response = await fetch(api);
-            const data = await response.json();
-
-            Storage.saveData(data);
-        } catch (e) {
-            console.log(e);
-        }
-    }
+  }
 }
 
 class Storage {
-    static saveData(data) {
-        localStorage.setItem("weather", JSON.stringify(data));
-    }
+  static saveData(data) {
+    localStorage.setItem("weather", JSON.stringify(data));
+  }
 
-    static getAllData() {
-        const data = JSON.parse(localStorage.getItem("weather"));
-
-        return data;
-    }
+  static getAllData() {
+    const data = JSON.parse(localStorage.getItem("weather"));
+    return data;
+  }
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    const ui = new UI();
+  const ui = new UI();
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(ui.getAPI);
-        ui.showUI(Storage.getAllData());
-    }
-
-
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(ui.getAPI);
+    ui.showUI(Storage.getAllData());
+  }
 });
